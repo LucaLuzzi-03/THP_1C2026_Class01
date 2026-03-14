7fb389605248abf38ccf6ea5e3b3d4568b7bc549
Luca Luzzi <luzziluca6@gmail.com>
2026-03-13 23:02:47 -0300

    Adding Exercise_07 resolved.

---
+++ src/thp/tp1/Exercise_07.java
@@ -0,0 +1,12 @@
+package thp.tp1;
+
+public class Exercise_07 {
+
+	public static void main(String[] args) {
+		// Realizá un programa que permita ingresar el ancho y el largo de un terreno en metros y el
+		// valor del metro cuadrado de tierra. Debemostrarse el valor total del terreno y la cantidad de
+		// metros de alambre para cercarlo completamente a tres alturas distintas.
+
+	}
+
+}
